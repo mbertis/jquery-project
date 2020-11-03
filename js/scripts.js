74a17b2f7742ebc03ee8c97a05e84307bb4b5672
mbertis <madisonbertis@gmail.com>
2020-11-03 13:00:44 -0800

    formatted scripts file with prettier

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,34 @@
 function newItem() {
+  let list = $("#list");
+  let li = $("<li></li>");
+  let inputValue = $("#input").val();
 
-let list = $("#list");
-let li = $("<li></li>");
-let inputValue = $("#input").val();
+  li.append(inputValue);
 
-li.append(inputValue);
-
-// Adds list item to list unless input field is left empty
-if (inputValue === '') {
+  // Adds list item to list unless input field is left empty
+  if (inputValue === "") {
     alert("You must write something");
-} else {
+  } else {
     list.append(li);
-}
+  }
 
-// Adds strike-through on double-click
-function crossOut() {
+  // Adds strike-through on double-click
+  function crossOut() {
     li.toggleClass("strike");
-}
+  }
 
-li.on("dblclick", crossOut);
+  li.on("dblclick", crossOut);
 
-let deleteButton = $("<deleteButton></deleteButton>");
-deleteButton.append(document.createTextNode("X"));
-li.append(deleteButton);
+  let deleteButton = $("<deleteButton></deleteButton>");
+  deleteButton.append(document.createTextNode("X"));
+  li.append(deleteButton);
 
-
-// Allows item to be deleted when "X" button is clicked
-deleteButton.on("click", deleteListItem);
-function deleteListItem() {
+  // Allows item to be deleted when "X" button is clicked
+  deleteButton.on("click", deleteListItem);
+  function deleteListItem() {
     li.addClass("delete");
-}
+  }
 
-// Allows for reordering items
-$("#list").sortable();
+  // Allows for reordering items
+  $("#list").sortable();
 }
